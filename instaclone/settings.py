caf3d58158e2c90ebbda53fa191dfbbee50c019e
jLuseno161 <joyluseno2@gmail.com>
2021-07-08 22:36:18 +0300

    create db & configure settings

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -73,12 +73,16 @@ WSGI_APPLICATION = 'instaclone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'instaclone',
+        'USER': 'moringa',
+    'PASSWORD':'jluseno',
     }
 }
+
 
 
 # Password validation
