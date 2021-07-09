7146d83e50020bc17661b81abb4c89d7fe3f8127
jLuseno161 <joyluseno2@gmail.com>
2021-07-09 12:39:34 +0300

    create user profile

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+
 
 # Create your models here.
+# class Article(models.Model):
+#     title = models.CharField(max_length=60)
+#     post = models.TextField()
+#     editor = models.ForeignKey(User,on_delete=models.CASCADE)
+#     tags = models.ManyToManyField(tags)
+#     pub_date = models.DateTimeField(auto_now_add=True)
+#     article_image = models.ImageField(upload_to='articles/', blank=True)
+
+class Profile(models.Model):
+    profile_pic = CloudinaryField('image')
+    bio = models.TextField(max_length=500)
+    
