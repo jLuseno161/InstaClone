e2e84df2caac9092406acf24f3f572e31f2dcc51
jLuseno161 <joyluseno2@gmail.com>
2021-07-12 07:14:22 +0300

    add class to admin

--- insta/admin.py
+++ insta/admin.py
@@ -1,6 +1,7 @@
-from insta.models import Image, Profile
+from insta.models import Comment, Image, Profile
 from django.contrib import admin
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Image)
+admin.site.register(Comment)
